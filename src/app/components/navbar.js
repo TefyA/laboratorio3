b754c5454d0edce099f94da5e6b38600ab02a360
Básicos CCB <Basicos@ccb.edu.gt>
2023-08-31 10:30:15 -0600

    Create navbar

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -5,21 +5,29 @@ import styles from './navbar.module.css';
  function Navbar(){
     const [selectedItem, setSelectedItem] = useState('Inicio');
 
-    const handleItemClick = (item) => {
+    const handleItemClick = (item, id) => {
         setSelectedItem(item);
+        scrollToSection(id);
+    };
+
+    const scrollToSection = (id) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
     };
 
     return (
         <nav className={styles.navbar}>
             <div
-                className={styles.navbarItem + `${selectedItem === 'Inicio' ? styles.selected: ''}`}
-                onClick={() => handleItemClick ('Inicio')}
+                className={styles.navbarItem + ` ${selectedItem === 'Inicio' ? styles.selected: ''}`}
+                onClick={() => handleItemClick ('Inicio', 'header')}
             >
                 <a href="#header">Inicio</a>
             </div>
             <div 
-                className={styles.navbarItem + `${selectedItem === 'Skills' ? styles.selected: ''}`}
-                onClick={() => handleItemClick ('Skills')}
+                className={styles.navbarItem + ` ${selectedItem === 'Skills' ? styles.selected: ''}`}
+                onClick={() => handleItemClick ('Skills', 'info')}
             >
                 <a href="#info">Servicios</a>
             </div>
